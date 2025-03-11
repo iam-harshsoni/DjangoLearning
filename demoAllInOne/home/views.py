f38f1fa64b7e1bf62fb357a6c67122d8a7394753
iam-harshsoni <harshsoni6011@gmail.com>
2025-03-11 18:08:21 +0530

    Completed CURD operation on Contactus page

--- demoAllInOne/home/views.py
+++ demoAllInOne/home/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from home.models import *
 from datetime import datetime
 from django.contrib import messages
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
@@ -18,30 +19,65 @@ def services(request):
 
     return render(request, "services.html")
 
-def contact(request):
+def contact(request, id=None):
     if not request.user.is_authenticated:
         return redirect("/login")
     
-    if request.method=="POST":
+    if request.method == "POST":
+        # id = request.POST.get("id")
         name = request.POST.get("name")
         email = request.POST.get("email")
         phone = request.POST.get("phone")
         desc = request.POST.get("desc")
-        contact = Contact(name=name, email=email, desc=desc, phone=phone, date=datetime.today())
 
-        contact.save()
-        messages.success(request, "Your message has been sent successfully!")
+        
+        if id is None:
+            # Create new contact
+            contact = Contact(name=name, email=email, desc=desc, phone=phone, date=datetime.today())
+            contact.save()
+            messages.success(request, "Your message has been sent successfully!")
+        else:
+            # Update existing contact
+            contact = Contact.objects.get(id = id)
+            contact.name = name
+            contact.email = email
+            contact.phone = phone
+            contact.desc = desc
+            contact.date = datetime.today()  # Optional: Update the date if needed
+            contact.save()
+            messages.success(request, "Your message has been updated successfully!")
+            return redirect("/contact")
 
-    return render(request, "contact.html")
+    context = {}
+    if id is not None:
+        queryset = Contact.objects.get(id = id)
+        context = {'contactDetail' : queryset}
+
+    return render(request, "contact.html", context)
+
+def contactList(request):
+    if not request.user.is_authenticated:
+        return redirect("/login")
+    
+    queryset = Contact.objects.all()
+    context={'contactList' : queryset}
+
+    return render(request, "contactList.html", context)
+
+def delete_contactList(request,id):
+    if(int(id) > 0):
+        queryset = Contact.objects.get(id=int(id))
+        queryset.delete()
+    return redirect("/contactList")
 
 def about(request):
     if not request.user.is_authenticated:
         return redirect("/login")
-    
     return render(request, "about.html")
 
-def loginUser(request):
 
+
+def loginUser(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
